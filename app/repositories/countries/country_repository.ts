c20f743e0b7c0d9570ee9a14141b421e6079945d
Igor Piedra <piedraigor@gmail.com>
2025-10-25 11:18:10 -0400

    Remove unused rawQuery

--- app/repositories/countries/country_repository.ts
+++ app/repositories/countries/country_repository.ts
@@ -10,17 +10,7 @@ type CountrySelectResult = {
 export default class CountryRepository {
 
   static async select(): Promise<CountrySelectResult[]> {
-    const query = `
-      SELECT
-        countries.id,
-        countries.name as text,
-        countries.nationality as nat,
-        countries.phone_code as prefix
-      FROM
-        countries
-      WHERE
-        countries.enabled = true
-    `
+
     const result = await db.from('countries').select(
       'countries.id',
       'countries.name as text',
