22916df5ec331d235e6c12075317404dbedb2eba
Igor Piedra <piedraigor@gmail.com>
2025-11-24 00:04:44 -0400

    Fix is_super down method

--- database/migrations/1763954462785_create_add_is_super_to_business_users_table.ts
+++ database/migrations/1763954462785_create_add_is_super_to_business_users_table.ts
@@ -13,17 +13,13 @@ export default class extends BaseSchema {
   }
 
   async down() {
-    const db = this.db
-    const hasColumn = async (table: string, column: string) => {
-      const result = await db.rawQuery(`SELECT 1 FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = ? AND COLUMN_NAME = ?`, [table, column])
-      return result[0].length > 0
-    }
 
-    const hasAuthorizer = await hasColumn('business_users', 'is_authorizer')
+
+    const hasAuthorizer = await this.schema.hasColumn('business_users', 'is_super')
 
     if (hasAuthorizer) {
       this.schema.alterTable('business_users', (table) => {
-        table.dropColumn('is_authorizer')
+        table.dropColumn('is_super')
       })
     }
   }
