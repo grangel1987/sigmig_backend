7aa5d3cc798707792cbbafcec61dd83fa99d47e1
Igor Piedra <piedraigor@gmail.com>
2025-11-28 18:58:14 -0400

    Fix client creation preloads

--- app/controllers/clients/client_controller.ts
+++ app/controllers/clients/client_controller.ts
@@ -178,7 +178,7 @@ export default class ClientController {
                     .preload('personalData', (pdQ) => pdQ.select('names', 'last_name_p', 'last_name_m'))
                     .select(['id', 'personal_data_id', 'email'])
             })
-            await client.load('typeIdentify', (builder) => builder.select(['id', 'text', 'client_contact_type_id']))
+            await client.load('typeIdentify', (builder) => builder.select(['id', 'text']))
             await client.load('city', (builder) => builder.select(['id', 'country_id']))
             await client.load('files')
             await client.load('documentInvoice')
