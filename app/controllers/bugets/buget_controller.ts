f0f5c7cb7760d340a86c6f22d5279324a51f7e0f
Igor Piedra <piedraigor@gmail.com>
2025-12-07 12:08:34 -0400

    fix budget nro filtering

--- app/controllers/bugets/buget_controller.ts
+++ app/controllers/bugets/buget_controller.ts
@@ -221,7 +221,7 @@ export default class BugetController {
 
 
     if (text) query = query
-      .whereRaw('nro LIKE ?', [`%${text}`])
+      .whereRaw('nro LIKE ?', [`${text}%`])
       .orWhereHas('client',
         (qb) => qb.whereRaw('name LIKE ?', [`%${text}%`]))
 
