9278d0ce9ae10a5877eb36554b362a55818d3340
Igor Piedra <piedraigor@gmail.com>
2026-04-30 07:10:58 -0400

    Implement logic to exclude newer shopping entries in queries

--- app/controllers/shoppings/shopping_controller.ts
+++ app/controllers/shoppings/shopping_controller.ts
@@ -109,6 +109,15 @@ export default class ShoppingController {
 
         if (status !== undefined) {
             query = query.where('enabled', status === 'enabled')
+        } else {
+            query = query.whereNotExists((builder) => {
+                builder
+                    .from('shoppings as newer_shoppings')
+                    .select(db.raw('1'))
+                    .whereColumn('newer_shoppings.business_id', 'shoppings.business_id')
+                    .whereColumn('newer_shoppings.nro', 'shoppings.nro')
+                    .whereColumn('newer_shoppings.id', '>', 'shoppings.id')
+            })
         }
 
         if (providerId) {
